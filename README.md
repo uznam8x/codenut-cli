5643ff0242eeb00048ee4370954db16c15458b39
jinam Yu <znu@thenet-global.com>
2017-12-15 18:19:32 +0900

    update generate

--- README.md
+++ README.md
@@ -11,6 +11,16 @@ npm install -g codenut-cli
 nut create <component>
 nut page <path>
 nut generate webfont <font-path> <dest>
+
+# mac os
+brew install imagemagick
+
+# window
+visit https://www.imagemagick.org/script/download.php
+download ImageMagick-7.0.7-14-Q16-x64-dll.exe 
+
+# image size : 1024 x 1024
+nut generate appicon <img-path>
 ```
 
 ### Example
@@ -19,4 +29,5 @@ nut create textfield
 nut page component/contact-us
 nut generate webfont 'font/heveltica.ttf' 'heveltica'
 nut generate webfont 'font/*.ttf' 'heveltica'
+nut generate appicon './icon.png'
 ```
--- bin/index.js
+++ bin/index.js
@@ -7,52 +7,229 @@ const nut = cli.arguments('<nut>');
 const fs = require('fs');
 const path = require('path');
 const concat = require('concat');
-const glob = require("glob");
+const glob = require('glob');
+
+// # INSTALL
 nut
   .command('install [comp]')
   .action((comp) => {
-      console.log('asdf');
+      console.log('coming soon');
     }
   )
 ;
 
-const appicon = (file) => {
+// # GENERATOR : ICON
+const async = require('async');
+const gm = require('gm').subClass({ imageMagick: true });
+const appicon = (file, dist) => {
   'use strict';
+
+  const config = {
+    ios: [
+      { size: 20 },
+      { size: 29 },
+      { size: 40 },
+      { size: 40 },
+      { size: 50 },
+      { size: 57 },
+      { size: 58 },
+      { size: 60 },
+      { size: 60 },
+      { size: 72 },
+      { size: 76 },
+      { size: 80 },
+      { size: 87 },
+      { size: 100 },
+      { size: 114 },
+      { size: 120 },
+      { size: 120 },
+      { size: 144 },
+      { size: 152 },
+      { size: 167 },
+      { size: 180 },
+      { size: 512 },
+      { size: 1024 },
+    ],
+    android: [
+      { size: 48 },
+      { size: 72 },
+      { size: 96 },
+      { size: 144 },
+      { size: 192 },
+      { size: 512 }
+    ],
+    web: [
+      { size: 16 },
+      { size: 32 },
+      { size: 48 },
+      { size: 64 },
+    ],
+    ms: [
+      { size: 70 },
+      { size: 150 },
+      { size: 310 },
+    ],
+  };
+
+  const meta = [];
+  for (let key in config) {
+    for (let i = 0, len = config[key].length; i < len; i++) {
+      meta.push({ size: config[key][i].size, platform: key });
+    }
+  }
+  let directory = 'app/prod/resource/icon/';
+
+  async.each(meta, (data, cb) => {
+    mkdir(directory + data.platform, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let fileName = data.size + 'x' + data.size;
+        if (data.platform === 'web') {
+          let output = directory + data.platform + '/' + fileName + '.ico';
+          gm(file)
+            .borderColor('white')
+            .border(0, 0)
+            .alpha('off')
+            .colors(256)
+            .resize(data.size, data.size)
+            .write(output, function (err) {
+              if (err) {
+                console.error(err);
+                return cb(err);
+              } else {
+                console.log('generated appicon : %s', output);
+                cb(null);
+              }
+            });
+
+        } else {
+          let output = directory + data.platform + '/' + fileName + '.png';
+          gm(file)
+            .background('none')
+            .noProfile()
+            .resize(data.size, data.size)
+            .gravity('Center')
+            .quality(100)
+            .write(output, function (err) {
+              if (err) {
+                console.error(err);
+                return cb(err);
+              } else {
+                console.log('generated appicon : %s', output);
+                cb(null);
+              }
+            });
+        }
+      }
+    });
+
+  }, (err, cb) => {
+    // complete
+    if (err) {
+      if (cb) return cb(err);
+      else throw err;
+    }
+
+    const link = '<link rel="{{rel}}" sizes="{{size}}" href="{{href}}" type="{{type}}" />';
+
+    let el = '';
+    for (let i = 0, len = config['ios'].length; i < len; i++) {
+      let fileName = config['ios'][i].size + 'x' + config['ios'][i].size;
+      el += link.replace(/{{rel}}/g, 'apple-touch-icon')
+        .replace(/{{size}}/g, fileName)
+        .replace(/{{type}}/g, 'image/png')
+        .replace(/{{href}}/g, '/resource/icon/ios/' + fileName + '.png') + '\n';
+    }
+
+    for (let i = 0, len = config['web'].length; i < len; i++) {
+      let fileName = config['web'][i].size + 'x' + config['web'][i].size;
+      el += link.replace(/{{rel}}/g, 'icon')
+        .replace(/{{size}}/g, fileName)
+        .replace(/{{type}}/g, 'image/x-icon')
+        .replace(/{{href}}/g, '/resource/icon/web/' + fileName + '.ico') + '\n';
+    }
+
+    el += '<link rel="manifest" href="/resource/icon/manifest.json" />';
+
+    mkdir('app/dev/element/', (err) => {
+      'use strict';
+      if (err) {
+        console.error(err);
+      } else {
+        let content = '';
+        if (fs.existsSync('app/dev/element/link.nunjucks')) {
+          content = fs.readFileSync('app/dev/element/link.nunjucks', 'utf-8');
+          content += '\n';
+        }
+        fs.writeFile('app/dev/element/link.nunjucks', content + el, 'utf-8', () => {
+          console.log('modify file : app/dev/element/link.nunjucks');
+        });
+
+        content = '';
+        if (fs.existsSync('app/dev/element/meta.nunjucks')) {
+          content = fs.readFileSync('app/dev/element/meta.nunjucks', 'utf-8');
+          content += '\n';
+        }
+
+        let meta = '<meta name="msapplication-config" content="/resource/icon/browserconfig.xml" />';
+        fs.writeFile('app/dev/element/meta.nunjucks', content + meta, 'utf-8', () => {
+          console.log('modify file : app/dev/element/meta.nunjucks');
+        });
+      }
+    });
+
+    let content = fs.readFileSync(path.resolve(__dirname, '../template/icon/browserconfig.xml'), 'utf-8');
+    fs.writeFile('app/prod/resource/icon/browserconfig.xml', content, 'utf-8', () => {
+      console.log('create file : app/prod/resource/icon/browserconfig.xml');
+    });
+
+    content = fs.readFileSync(path.resolve(__dirname, '../template/icon/manifest.json'), 'utf-8');
+    fs.writeFile('app/prod/resource/icon/manifest.json', content, 'utf-8', () => {
+      console.log('create file : app/prod/resource/icon/manifest.json');
+    });
+
+    if (cb) {
+      cb(null);
+    }
+  });
 };
 
+// # GENERATOR : WEB FONT
 const Fontmin = require('fontmin');
-const webfont = (src, dest) => {
+const webfont = (src, dist) => {
   'use strict';
 
-  dest = ('app/prod/resource/font/' + dest).replace(/\/\//g, '/');
+  dist = ('app/prod/resource/font/' + dist).replace(/\/\//g, '/');
 
-  let fontmin = new Fontmin()
-    .src(src)
-    .dest(dest);
+  let fontmin = new Fontmin();
+  fontmin.src(src).dest(dist);
 
   fontmin.run(function (err, files) {
     if (err) throw err;
-    glob(dest + '/*.css', null, (err, arr) => {
+    glob(dist + '/*.css', null, (err, arr) => {
       if (err) throw err;
-      concat(arr, dest + '/base.css');
+      concat(arr, dist + '/base.css');
     });
 
-    console.log('generate webfont : ' + dest);
+    console.log('generated webfont : ' + dist);
   });
 };
 
 nut
-  .command('generate [type] [file] [dest]')
-  .action((type, file, dest) => {
+  .command('generate [type] [file] [dist]')
+  .action((type, file, dist) => {
       if (type === 'webfont') {
-        webfont(file, dest);
+        webfont(file, dist);
       } else if (type === 'appicon') {
-        appicon(file, dest);
+        appicon(file);
       }
     }
   )
 ;
 
+
+// # PAGE
 nut
   .command('page [location]')
   .action((location) => {
@@ -80,14 +257,16 @@ nut
   )
 ;
 
+// # LAYOUT
 nut
   .command('layout [comp]')
   .action((comp) => {
-      console.log('layout');
+      console.log('coming soom');
     }
   )
 ;
 
+// # NUT
 nut
   .command('create [comp]')
   .action((comp) => {
