f787dfe05c73d5d8692e078fc6c54e4fb70fe637
jinam Yu <znu@thenet-global.com>
2017-12-20 19:27:19 +0900

    update install

--- bin/index.js
+++ bin/index.js
@@ -3,17 +3,91 @@ const cli = require('commander');
 const chalk = require('chalk');
 const Progress = require('progress');
 const mkdir = require('mkdirp');
+const rmdir = require('rmdir');
 const nut = cli.arguments('<nut>');
 const fs = require('fs');
 const path = require('path');
 const concat = require('concat');
 const glob = require('glob');
 
+const co = require('co');
+const prompt = require('co-prompt');
+const request = require('request');
+const extract = require('extract-zip');
+const unzip = require('unzip');
+
 // # INSTALL
+const remote = 'http://api.codenut.local.com';
+const download = (data) => {
+  'use strict';
+
+  const file = data.url;
+  const name = data.name;
+  const type = data.type;
+  const base = 'app';
+  const output = {
+    nut: base + '/dev/nut/' + name + '/',
+    font: base + '/prod/resource/font/' + name + '/',
+  };
+
+  mkdir('temporary', (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let stream = fs.createWriteStream('temporary/' + file);
+      request(remote + '/download?file=' + file)
+        .on('response', (response) => {
+          //console.log(parseInt(response.headers['content-length'], 10));
+        })
+        .on('data', (chunk) => {
+          //console.log(chunk.length);
+          stream.write(chunk);
+        })
+        .on('end', () => {
+          stream.end();
+          const location = path.resolve('.') + '/';
+          const dir = location + output[type];
+          extract('./temporary/' + file, { dir: dir }, function (err) {
+            if (err) throw err;
+            fs.unlink('./temporary/' + file);
+            rmdir('./temporary');
+            console.log('asset installed : ' + dir);
+          });
+        });
+    }
+  });
+};
+
 nut
-  .command('install [comp]')
-  .action((comp) => {
-      console.log('coming soon');
+  .command('install [asset]')
+  .action((asset) => {
+      request({
+        url: remote + '/asset',
+        method: 'post',
+        body: { name: asset },
+        json: true,
+      }, (err, response, body) => {
+        'use strict';
+        if (body.code === 200) {
+          download(body.data);
+        } else if (body.code === 401) {
+          // Todo auth;
+
+        } else {
+          console.log('Not found asset');
+        }
+      });
+
+      /*
+        co(function* () {
+            'use strict';
+            const user = yield prompt('user : ');
+            const password = yield prompt('pasword : ');
+            console.log(user, password);
+            process.exit(0);
+          }
+        )
+        */
     }
   )
 ;
@@ -56,7 +130,7 @@ const appicon = (file, dist) => {
       { size: 96 },
       { size: 144 },
       { size: 192 },
-      { size: 512 }
+      { size: 512 },
     ],
     web: [
       { size: 16 },
@@ -77,6 +151,7 @@ const appicon = (file, dist) => {
       meta.push({ size: config[key][i].size, platform: key });
     }
   }
+
   let directory = 'app/prod/resource/icon/';
 
   async.each(meta, (data, cb) => {
@@ -162,6 +237,7 @@ const appicon = (file, dist) => {
           content = fs.readFileSync('app/dev/element/link.nunjucks', 'utf-8');
           content += '\n';
         }
+
         fs.writeFile('app/dev/element/link.nunjucks', content + el, 'utf-8', () => {
           console.log('modify file : app/dev/element/link.nunjucks');
         });
@@ -228,7 +304,6 @@ nut
   )
 ;
 
-
 // # PAGE
 nut
   .command('page [location]')
@@ -288,7 +363,7 @@ nut
                 console.log('create nut : ' + location);
               });
             });
-          })
+          });
         }
       });
     }
